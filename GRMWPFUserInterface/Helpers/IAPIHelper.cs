e871afdac893cdfa8bc8706a3f63d4f0148c2a49
Glenn Goossens <glenngoossens07@gmail.com>
2020-01-05 13:01:28 +0100

    Login Form and DB adjustmend

---
+++ GRMWPFUserInterface/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using GRMWPFUserInterface.Models;
+using System.Threading.Tasks;
+
+namespace GRMWPFUserInterface.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
